9abd40c791f839985c0404ce40fc3c04715d895d
Vinay Ganireddy <vinayreddy1616@gmail.com>
2025-11-26 19:46:48 +0530

    commit2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,14 +8,16 @@ const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <Col md={6} className="text-center text-md-start mb-3 mb-md-0">
-            <h5>SHOE MART</h5>
-            <p className="small mb-0">© 2025 All Rights Reserved</p>
-          </Col>
-          <Col md={6} className="text-center text-md-end">
-            <a href="#" className="text-white mx-2"><Facebook /></a>
-            <a href="#" className="text-white mx-2"><Instagram /></a>
-            <a href="#" className="text-white mx-2"><Twitter /></a>
-          </Col>
+  <h5>SHOE MART</h5>
+  <p className="small mb-0">© 2025 All Rights Reserved</p>
+</Col>
+
+<Col md={6} className="text-center text-md-end">
+  <a href="/" className="text-white mx-2"><Facebook /></a>
+  <a href="/" className="text-white mx-2"><Instagram /></a>
+  <a href="/" className="text-white mx-2"><Twitter /></a>
+</Col>
+
         </Row>
       </Container>
     </footer>
